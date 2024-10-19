290dc559be132c485d13c0340d2a99cba7ede572
Mustapha7018 <adamsmustapha419@gmail.com>
2024-10-19 00:59:35 +0000

    configure twilio and add delete contact functionality

--- whatsapp_project/messaging/tests.py
+++ whatsapp_project/messaging/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+# # test_twilio.py
+# from twilio.rest import Client
+# import os
+# from dotenv import load_dotenv
 
-# Create your tests here.
+# load_dotenv()
+
+# account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+# auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+
+# client = Client(account_sid, auth_token)
+
+# try:
+#     client.api.accounts(account_sid).fetch()
+#     print("Twilio credentials are valid.")
+# except Exception as e:
+#     print("Twilio credentials are invalid:", e)
+
